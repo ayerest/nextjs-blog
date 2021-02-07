cd194cc0ccf93470a96bb8acf5c43f0bcd290306
ayerest <irisiseve@gmail.com>
2021-02-07 14:02:15 -0800

    adding image

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 const FirstPost = () => {
   return (
@@ -8,6 +9,12 @@ const FirstPost = () => {
         <Link href="/">
           <a>Back to home</a>
         </Link>
+        <Image
+          src="/images/profile.jpg" // Route of the image file
+          height={144} // Desired size with correct aspect ratio
+          width={144} // Desired size with correct aspect ratio
+          alt="Your Name"
+        />
       </h2>
     </>
   )
